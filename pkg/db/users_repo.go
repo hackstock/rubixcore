62293d9a30bc1a7d6cd9d822c269a0c26ae56779
= <=>
2018-11-03 12:05:52 +0100

    adds method for authenticating users and updating last login data

--- pkg/db/users_repo.go
+++ pkg/db/users_repo.go
@@ -28,6 +28,28 @@ func NewUsersRepo(db *sqlx.DB) *UsersRepo {
 	return &UsersRepo{db}
 }
 
+// Authenticate returns a user with the specified login credentials
+func (repo *UsersRepo) Authenticate(username, password string) (*UserAccount, error) {
+	query := "SELECT * FROM user_accounts AS u WHERE u.username = ? AND u.password = ?"
+
+	u := new(UserAccount)
+	err := repo.db.QueryRowx(query, username, password).StructScan(u)
+	if err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+// UpdateLastLogin updates the last login date for the specified user
+func (repo *UsersRepo) UpdateLastLogin(id int) error {
+	query := "UPDATE user_accounts SET last_login_at = CURRENT_TIMESTAMP() WHERE id = ?"
+
+	_, err := repo.db.Exec(query, id)
+
+	return err
+}
+
 // Create saves a UserAccount into the database
 func (repo *UsersRepo) Create(u *UserAccount) error {
 	query := "INSERT INTO user_accounts (username, password, is_admin) VALUES (?, ?, ?)"
--- pkg/db/users_repo_test.go
+++ pkg/db/users_repo_test.go
@@ -184,3 +184,133 @@ func TestGetAll_ShouldFail(t *testing.T) {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
+
+func TestAuthenticate_ShouldPass(t *testing.T) {
+	query := `^SELECT \* FROM user_accounts AS u WHERE u.username = \? AND u.password = \?$`
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	username := "someuser"
+	password := "somepassword"
+
+	mock.ExpectQuery(query).WithArgs(
+		username,
+		password,
+	).WillReturnRows(
+		sqlmock.NewRows([]string{"id", "username", "password", "is_admin", "created_at", "last_login_at", "updated_at"}).
+			AddRow(1, username, "$2a$10$pJofeBaFtdXo4RdRrKBJF.FW/ePvnS3.xgNpdC0N4FNt2S1H3QO2K", false, time.Now(), nil, nil),
+	)
+
+	dbMock := sqlx.NewDb(db, "sqlmock")
+	repo := NewUsersRepo(dbMock)
+
+	_, err = repo.Authenticate(username, password)
+	if err != nil {
+		t.Fatalf("expected not error, got %v", err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestAuthenticate_ShouldFail(t *testing.T) {
+	query := `^SELECT \* FROM user_accounts AS u WHERE u.username = \? AND u.password = \?$`
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	username := "someuser"
+	password := "somepassword"
+
+	mock.ExpectQuery(query).WithArgs(
+		username,
+		password,
+	).
+		WillReturnError(fmt.Errorf("db error"))
+
+	dbMock := sqlx.NewDb(db, "sqlmock")
+	repo := NewUsersRepo(dbMock)
+
+	user, err := repo.Authenticate(username, password)
+	if err == nil {
+		t.Fatalf("expected error, got none")
+	}
+
+	if user != nil {
+		t.Fatalf("expected nil, got %v", user)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestUpdateLastLogin_ShouldPass(t *testing.T) {
+	query := `^UPDATE user_accounts SET last_login_at = CURRENT_TIMESTAMP\(\) WHERE id = \?$`
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	id := 1
+	mock.ExpectExec(query).
+		WithArgs(
+			id,
+		).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	dbMock := sqlx.NewDb(db, "sqlmock")
+	repo := NewUsersRepo(dbMock)
+
+	err = repo.UpdateLastLogin(id)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestUpdateLastLogin_ShouldFail(t *testing.T) {
+	query := `^UPDATE user_accounts SET last_login_at = CURRENT_TIMESTAMP\(\) WHERE id = \?$`
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	id := 1
+	mock.ExpectExec(query).
+		WithArgs(
+			id,
+		).
+		WillReturnError(fmt.Errorf("db error"))
+
+	dbMock := sqlx.NewDb(db, "sqlmock")
+	repo := NewUsersRepo(dbMock)
+
+	err = repo.UpdateLastLogin(id)
+	if err == nil {
+		t.Fatalf("expected error, got none")
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
