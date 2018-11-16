83868e5f8a5d8baf04f23e542e612a216d2709cc
hackstock <hackstockpie@gmail.com>
2018-11-16 21:32:53 +0100

    new queues should be active

--- migrations/00_create_users_up.sql
+++ migrations/00_create_users_up.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS user_accounts
     id              INT            NOT NULL     AUTO_INCREMENT,
     username        VARCHAR(255)   NOT NULL,
     password        VARCHAR(255)   NOT NULL,
-    is_admin        BOOLEAN        DEFAULT FALSE,
+    is_admin        BOOLEAN        DEFAULT TRUE,
     created_at      DATETIME       DEFAULT NOW(),
     last_login_at   DATETIME       NULL,
     updated_at      DATETIME       NULL,      
